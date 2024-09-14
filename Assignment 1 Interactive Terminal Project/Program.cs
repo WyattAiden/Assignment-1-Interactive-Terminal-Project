b163e65a93123b125e83df34a678225df1f863dc
wasob <wasob@LAPTOP-BTQJG6R0>
2024-09-13 20:26:42 -0400

    Add project files.

---
+++ Assignment 1 Interactive Terminal Project/Program.cs
@@ -0,0 +1,194 @@
+﻿Console.WriteLine("Hello I'm Anton here are your things you can ask me \n1.tell me a joke\n2.write an adision question using numbers from 1 to 9");
+string choice = Console.ReadLine();
+if (choice == "tell me a joke")
+{
+    Console.WriteLine("Why are C# developers so good at playing hide-and-seek? Because they're experts in encapsulation:]");
+}
+else if (choice ==  "what is 1 plus 1")
+{
+    Console.WriteLine($"The sum of 1 and 1 is '{1 + 1}'");
+}
+else if (choice == "what is 1 plus 2")
+{
+    Console.WriteLine($"The sum of 1 and 1 is '{1 + 2}'");
+}
+else if (choice == "what is 1 plus 3")
+{
+    Console.WriteLine($"The sum of 1 and 3 is '{1 + 3}'");
+}
+else if (choice == "what is 1 plus 4")
+{
+    Console.WriteLine($"The sum of 1 and 4 is '{1 + 4}'");
+}
+else if (choice == "what is 1 plus 5")
+{
+    Console.WriteLine($"The sum of 1 and 5 is '{1 + 5}'");
+}
+else if (choice == "what is 1 plus 6")
+{
+    Console.WriteLine($"The sum of 1 and 6 is '{1 + 6}'");
+}
+else if (choice == "what is 1 plus 7")
+{
+    Console.WriteLine($"The sum of 1 and 7 is '{1 + 7}'");
+}
+else if (choice == "what is 1 plus 8")
+{
+    Console.WriteLine($"The sum of 1 and 8 is '{1 + 8}'");
+}
+else if (choice == "what is 1 plus 9")
+{
+    Console.WriteLine($"The sum of 1 and 9 is '{1 + 9}'");
+}
+else if (choice == "what is 2 plus 1")
+{
+    Console.WriteLine($"The sum of 2 and 1 is '{2 + 1}'");
+}
+else if (choice == "what is 2 plus 2")
+{
+    Console.WriteLine($"The sum of 2 and 2 is '{2 + 2}'");
+}
+else if (choice == "what is 2 plus 3")
+{
+    Console.WriteLine($"The sum of 2 and 3 is '{2 + 3}'");
+}
+else if (choice == "what is 2 plus 4")
+{
+    Console.WriteLine($"The sum of 2 and 4 is '{2 + 4}'");
+}
+else if (choice == "what is 2 plus 5")
+{
+    Console.WriteLine($"The sum of 2 and 5 is '{2 + 5}'");
+}
+else if (choice == "what is 2 plus 6")
+{
+    Console.WriteLine($"The sum of 2 and 6 is '{2 + 6}'");
+}
+else if (choice == "what is 2 plus 7")
+{
+    Console.WriteLine($"The sum of 2 and 7 is '{2 + 7}'");
+}
+else if (choice == "what is 2 plus 8")
+{
+    Console.WriteLine($"The sum of 2 and 8 is '{2 + 8}'");
+}
+else if (choice == "what is 2 plus 9")
+{
+    Console.WriteLine($"The sum of 2 and 9 is '{2 + 9}'");
+}
+else if (choice == "what is 3 plus 1")
+{
+    Console.WriteLine($"The sum of 3 and 1 is '{3 + 1}'");
+}
+else if (choice == "what is 3 plus 2")
+{
+    Console.WriteLine($"The sum of 3 and 2 is '{3 + 2}'");
+}
+else if (choice == "what is 3 plus 3")
+{
+    Console.WriteLine($"The sum of 3 and 3 is '{3 + 3}'");
+}
+else if (choice == "what is 3 plus 4")
+{
+    Console.WriteLine($"The sum of 3 and 4 is '{3 + 4}'");
+}
+else if (choice == "what is 3 plus 5")
+{
+    Console.WriteLine($"The sum of 3 and 5 is '{3 + 5}'");
+}
+else if (choice == "what is 3 plus 6")
+{
+    Console.WriteLine($"The sum of 3 and 6 is '{3 + 6}'");
+}
+else if (choice == "what is 3 plus 7")
+{
+    Console.WriteLine($"The sum of 3 and 7 is '{3 + 7}'");
+}
+else if (choice == "what is 3 plus 8")
+{
+    Console.WriteLine($"The sum of 3 and 8 is '{3 + 8}'");
+}
+else if (choice == "what is 3 plus 9")
+{
+    Console.WriteLine($"The sum of 3 and 9 is '{3 + 9}'");
+}
+else if (choice == "what is 4 plus 1")
+{
+    Console.WriteLine($"The sum of 4 and 1 is '{4 + 1}'");
+}
+else if (choice == "what is 4 plus 2")
+{
+    Console.WriteLine($"The sum of 4 and 2 is '{4 + 2}'");
+}
+else if (choice == "what is 4 plus 2")
+{
+    Console.WriteLine($"The sum of 4 and 2 is '{4 + 2}'");
+}
+else if (choice == "what is 4 plus 3")
+{
+    Console.WriteLine($"The sum of 4 and 3 is '{4 + 3}'");
+}
+else if (choice == "what is 4 plus 4")
+{
+    Console.WriteLine($"The sum of 4 and 4 is '{4 + 4}'");
+}
+else if (choice == "what is 4 plus 5")
+{
+    Console.WriteLine($"The sum of 4 and 5 is '{4 + 5}'");
+}
+else if (choice == "what is 4 plus 6")
+{
+    Console.WriteLine($"The sum of 4 and 6 is '{4 + 6}'");
+}
+else if (choice == "what is 4 plus 7")
+{
+    Console.WriteLine($"The sum of 4 and 7 is '{4 + 7}'");
+}
+else if (choice == "what is 4 plus 8")
+{
+    Console.WriteLine($"The sum of 4 and 8 is '{4 + 8}'");
+}
+else if (choice == "what is 4 plus 9")
+{
+    Console.WriteLine($"The sum of 4 and 9 is '{4 + 9}'");
+}
+else if (choice == "what is 5 plus 1")
+{
+    Console.WriteLine($"The sum of 5 and 1 is '{5 + 1}'");
+}
+else if (choice == "what is 5 plus 2")
+{
+    Console.WriteLine($"The sum of 5 and 2 is '{5 + 2}'");
+}
+else if (choice == "what is 5 plus 3")
+{
+    Console.WriteLine($"The sum of 5 and 3 is '{5 + 3}'");
+}
+else if (choice == "what is 5 plus 4")
+{
+    Console.WriteLine($"The sum of 5 and 4 is '{5 + 4}'");
+}
+else if (choice == "what is 5 plus 5")
+{
+    Console.WriteLine($"The sum of 5 and 5 is '{5 + 5}'");
+}
+else if (choice == "what is 5 plus 6")
+{
+    Console.WriteLine($"The sum of 5 and 6 is '{5 + 6}'");
+}
+else if (choice == "what is 5 plus 7")
+{
+    Console.WriteLine($"The sum of 5 and 7 is '{5 + 7}'");
+}
+else if (choice == "what is 5 plus 8")
+{
+    Console.WriteLine($"The sum of 5 and 8 is '{5 + 8}'");
+}
+else if (choice == "what is 5 plus 9")
+{
+    Console.WriteLine($"The sum of 5 and 9 is '{5 + 9}'");
+}
+else if (choice == "what is 5 plus 1")
+{
+    Console.WriteLine($"The sum of 5 and 1 is '{5 + 1}'");
+}
